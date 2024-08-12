0c72d4890200041efb0a70627b033f9544fb08d8
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-08-12 20:32:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-
-app.use(express.static("public"));
 //Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
@@ -39,13 +35,6 @@ app.post("/submit", (req,res) => {
 
   
 }); 
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
